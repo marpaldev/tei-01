8f37f08c9a149254c2a4cd99a30771382ce33ec1
marpaldev <marcospauloalve16@gmail.com>
2026-05-05 11:20:22 -0300

    Adição de repetições na saída

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def saudacao(nome):
-    return f"Olá, {nome}. Bem-vindo ao programa."
+def saudacao(nome: str, repet: int = 0) -> str:
+    if not repet == 0:
+        r = 0
+        while r < repet:
+            print(f"Olá, {nome}. Bem-vindo ao programa.")
+            r += 1
+    else:
+        print(f"Olá, {nome}. Bem-vindo ao programa.")
 
 if __name__ == "__main__":
-    print(saudacao("Mundo"))
+    saudacao("Mundo", 5)
